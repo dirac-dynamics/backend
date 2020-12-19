67b4677c7ca4c7d19f1a22d0d8491b79908d5dec
stubbi <stubbemann@cargoboard.de>
2020-12-19 16:18:10 +0100

    impliment simple carrier, transportable CRUD

--- app/dirac_django/settings.py
+++ app/dirac_django/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'rest_framework',
+    'rest_framework_gis',
     'route_optimization'
 ]
 
@@ -132,3 +134,8 @@ USE_TZ = True
 
 STATIC_URL = "/staticfiles/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
--- app/route_optimization/views.py
+++ app/route_optimization/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from .models import Carrier, Transportable
+from .serializers import CarrierSerializer, TransportableSerializer
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+
+class CarrierViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Carrier.objects.all().order_by('-id')
+    serializer_class = CarrierSerializer
+    permission_classes = [permissions.AllowAny]
+
+class TransportableViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Transportable.objects.all().order_by('-id')
+    serializer_class = TransportableSerializer
+    permission_classes = [permissions.AllowAny]
