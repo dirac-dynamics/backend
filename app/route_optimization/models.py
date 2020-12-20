388799926f6ff7fdb49037c3269f9cfc541df4cd
stubbi <stubbemann@cargoboard.de>
2020-12-20 20:47:53 +0100

    extend carrier and transportable model

--- app/route_optimization/models.py
+++ app/route_optimization/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.gis.db.models import PointField
 
-# Create your models here.
 class Carrier(models.Model):
     position = PointField()
+    phone = models.CharField(max_length=20)
+    driver = models.CharField(max_length=100)
+    plate_number = models.CharField(max_length=10)
 
 class Transportable(models.Model):
     position = PointField()
-    destination = PointField()
+    sender = models.CharField(max_length=200)
+    receiver = models.CharField(max_length=200)
