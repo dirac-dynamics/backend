7171e2bb4b72f37b7757af1a09f4fc692dc10047
stubbi <stubbemann@cargoboard.de>
2020-12-21 03:05:09 +0100

    it works!

--- app/dirac_django/settings.py
+++ app/dirac_django/settings.py
@@ -15,8 +15,8 @@ import os
 from glob import glob
 
 # Set GDAL Path for Postgis
-GDAL_LIBRARY_PATH = glob('/usr/lib/libgdal.so.*')[0]
-GEOS_LIBRARY_PATH = glob('/usr/lib/libgeos_c.so.*')[0]
+#GDAL_LIBRARY_PATH = glob('/usr/lib/libgdal.so.*')[0]
+#GEOS_LIBRARY_PATH = glob('/usr/lib/libgeos_c.so.*')[0]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- app/route_optimization/views.py
+++ app/route_optimization/views.py
@@ -4,6 +4,9 @@ from rest_framework import views, viewsets
 from rest_framework import permissions
 from rest_framework.response import Response
 
+from .graph import create_from_city
+
+
 
 class CarrierViewSet(viewsets.ModelViewSet):
     """
@@ -35,6 +38,13 @@ class MatcherViewSet(viewsets.ModelViewSet):
         carrier_positions = [c.position.coords for c in carriers]
         transportable_positions = [t.position.coords for t in transportables]
 
-        # optimization goes here
+        print(carrier_positions)
+        print(transportable_positions)
 
-        return Response({'yes':'no'})
+        # optimization goes here
+        carrier_to_transportable_optimal, routes_optimal, carrier_to_transportable_greedy, routes_greedy  = create_from_city('Munich_2', 48.143743 + 0.05, 48.143743 - 0.05, 11.575942 + 0.10, 11.575942 - 0.10, len(carriers), len(transportables), random=False,carrier_list=carrier_positions, transportable_list=transportable_positions)
+
+        return Response({'carrier_to_transport_optimal':carrier_to_transportable_optimal,
+                         'routes_optimal':routes_optimal,
+                         'carrier_to_transport_greedy':carrier_to_transportable_greedy,
+                         'routes_greedy':routes_greedy})
